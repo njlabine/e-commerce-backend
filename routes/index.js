424854d939afb2af8a74378e2ff0f692224eb7df
Nate LaBine <njlabine@gmail.com>
2022-07-07 19:34:22 -0400

    fixing to async

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
+const categoryRoutes = require('./api/category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
